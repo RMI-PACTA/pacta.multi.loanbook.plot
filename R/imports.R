5ef25928108ee76a7bdf6603d28e5470e0faf53b
jacobvjk <jacob.kastl@gmail.com>
2024-03-12 14:06:59 +0100

    import pipe from magrittr

--- R/imports.R
+++ R/imports.R
@@ -1,11 +1,6 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
 #' @importFrom magrittr %>%
 #' @importFrom rlang :=
 #' @importFrom rlang .data
 #' @importFrom rlang .env
 #' @importFrom rlang %||%
-## usethis namespace: end
 NULL
